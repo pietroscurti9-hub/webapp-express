41b7453ccc553e2b49983c71373138028128405b
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-24 18:09:20 +0100

    restyle, immagini con map aggunte

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express()
+const cors = require("cors"); 
 const port = 3000
 
 
 // importo i router
-
 const router = require('./routers/routerMovie');
 
+app.use(cors({
+origin: 'http://localhost:5173' 
+}));
+
 app.use('/api/movies', router)
+
 
 
 
